71e1a7b510902ae53c1dda1e3706db9604ded489
preethiv98 <preethiv98@outlook.com>
2019-09-16 21:31:22 -0400

    Added placeholder scripts that were created by instructor.

---
+++ Meteor Storm/Assets/Scripts/NPC.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    private Text message;
+    private bool notHit;
+    void Start()
+    {
+        //Create Text component of a canvas object through unity named "Message"
+        message = GameObject.Find("Message").GetComponent<Text>();
+        //message.color = Color.white;
+        message.text = "";
+        notHit=true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && notHit)
+        {
+            StartCoroutine(displayText(other));
+
+        }
+        //Message to display if main dialogue is done
+        else if(other.gameObject.tag == "Player" && !notHit)
+        {
+            message.text = "You Are Going The Wrong Way";
+        }
+    }
+    IEnumerator displayText(Collider2D other)
+    {
+        message.text = "Be Careful As You Move Forward";
+        other.gameObject.GetComponent<Move2D>().speed = 0;
+        //will continue after mouse button is clicked
+        //copy this block for every new line of dialouge
+        while (!Input.GetMouseButtonDown(0)) {
+        yield return null;
+        }
+        message.text = "Watch out for dragons!";
+        //
+
+
+
+        //let player move again
+        other.gameObject.GetComponent<Move2D>().speed = 5;
+        notHit=false;
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            message.text = "";
+        }
+    }
+}
